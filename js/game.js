93d33fc70e79abcc90a502e9447cd0eac88b63e2
Aahan Gandhi <72563048+aahangandhi@users.noreply.github.com>
2022-01-04 13:24:17 +0300

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,65 @@
+class Game {
+    constructor(){
+  
+    }
+  
+ 
+    update(state){
+      database.ref('/').update({
+        gameState: state
+      });
+    }
+  
+    async start(){
+
+
+    }
+  
+    play(){
+      
+      if(allPlayers !== undefined){
+        background(rgb(198,135,103));
+        image(track, 0,-displayHeight*4,displayWidth, displayHeight*5);
+        
+        //var display_position = 100;
+        
+        //index of the array
+        var index = 0;
+  
+        //x and y position of the cars
+        var x = 175 ;
+        var y;
+  
+        drawSprites();
+  
+        for(var plr in allPlayers){
+          //add 1 to the index for every loop
+          index = index + 1 ;
+  
+          //position the cars a little away from each other in x direction
+          x = x + 200;
+          //use data form the database to display the cars in y direction
+          y = displayHeight - allPlayers[plr].distance;
+          cars[index-1].x = x;
+          cars[index-1].y = y;
+  
+          if (index === player.index){
+            stroke(10);
+            fill("red");
+            ellipse(x,y,60,60);
+            cars[index - 1].shapeColor = "red";
+            camera.position.x = displayWidth/2;
+            camera.position.y = cars[index-1].y;
+          }
+        }
+  
+      }
+  
+     
+     
+      drawSprites();
+    }
+  
+    
+  }
+  
